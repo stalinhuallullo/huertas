3765099d9db5b4aec08f035566c41433964dfca2
StalinHuallullo <antonyhuallullo@gmail.com>
2021-08-15 00:51:31 -0500

    commit de prueba

--- resources/views/web/layouts/index.blade.php
+++ resources/views/web/layouts/index.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+  
     @if(isset($property_single) && isset($cache_company))
         <title>{{ $property_single->name }} | {{ $cache_company->name }}</title>
         <meta name="description" content="{{ $property_single->description }}">
